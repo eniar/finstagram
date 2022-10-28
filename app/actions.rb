3ce670149670282a0e7a5edc31c3320b62eb7a61
eniar <kreeraine@gmail.com>
2022-10-28 14:56:55 +0000

    made changes

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,16 @@
-get "/" do
-    File.read(File.join("app/views", "index.html"))
+def humanized_time_ago(minute_num)
+  if minute_num >= 60
+    "#{minute_num / 60} hours ago"
+  else
+    "#{minute_num} minutes ago"
   end
+end
+
+get '/' do
+  # your previous finstagram post code is up here
+  # add the following @finstagram_posts line
+
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+
+  erb(:index)
+end
